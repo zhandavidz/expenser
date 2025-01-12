f1464472d88204fbdca205f7255c5c4f4ee32e7a
David Zhan <zhandavidz@gmail.com>
2025-01-12 03:57:32 -0800

    add everything for the basics of having expressjs in the repo

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
